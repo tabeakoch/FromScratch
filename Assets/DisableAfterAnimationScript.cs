23fb882106b83ebaeb454744165014ba06aa2922
Tabea Koch <tabea.koch@innogames.com>
2016-07-11 18:41:40 +0200

    mainly animations

---
+++ Assets/DisableAfterAnimationScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableAfterAnimationScript
+: MonoBehaviour {
+	public Animator EquipmentSlotAnimator; 
+
+	private bool open = false;
+
+	public void OnSignifierGrabClick (){
+		
+			EquipmentSlotAnimator.SetTrigger ("fadeout");
+			StartCoroutine (DisableAfterAnimation());
+
+			
+	}
+
+	IEnumerator DisableAfterAnimation () {
+		yield return new WaitForSeconds (1.5f);
+		this.gameObject.SetActive (false);
+	}
+}
+ 
